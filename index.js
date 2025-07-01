32bbe627ffac14e5137f86646e85b5d3570e0d5c
downhillthursday <adhirajmohanta@gmail.com>
2025-07-01 18:31:11 +0530

    addy

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import express from 'express';
 import cors from 'cors';
 import axios from 'axios';
 import dotenv from 'dotenv';
-
+dotenv.config();
 // -------------------- App Setup --------------------
 const app = express();
 const PORT = process.env.PORT || 3001;
